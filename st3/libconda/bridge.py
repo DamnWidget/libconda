64023d217c13c690e224b6512996caf6f0ac9755
Oscar Campos <damnwidget@gmail.com>
2018-03-18 14:19:19 +0000

    add prometheus statistics gathering for GolcondaBridge

--- st3/libconda/bridge.py
+++ st3/libconda/bridge.py
@@ -1,13 +1,27 @@
 import requests
 
 from .packages.typing import Dict
+from .packages.prometheus_client import Summary, Counter
+
+REQUEST_TIME = Summary(
+    'golconda_request_processing_seconds',
+    'Time spent processing requests from ST3 plugins to golconda server'
+)
+
+REQUESTS_COUNT = Counter(
+    'golconda_requests_total', 'Total number of requests performed'
+)
+
+REQUESTS_EXCEPTIONS = Counter(
+    'golconda_tequests_exceptions', 'Number of raised exceptions on requests'
+)
 
 
 class GolcondaBridge:
     """Bridge between ST3 and Golconda's standalone service
     """
 
-    def __init__(self, hostname: str, port: int):
+    def __init__(self, hostname: str, port: int) -> None:
         self.__hostname = hostname
         self.__port = port
 
@@ -18,11 +32,17 @@ class GolcondaBridge:
 
         return 'http://{}:{}'.format(self.__hostname, self.__port)
 
+    @REQUEST_TIME.time()
     def request(self, end: str, pl: Dict, ttl: float=0.1) -> requests.Response:
         """Make a request to the Golconda service and return it's result
 
         Requests to Golconda are always POST (Except for instrumentation)
         """
 
-        endpoint = '{}/{}'.format(self.hostname, end)
-        return requests.post(endpoint, json=pl, timeout=ttl)
+        REQUESTS_COUNT.inc()
+
+        with REQUESTS_EXCEPTIONS.count_exceptions():
+            endpoint = '{}/{}'.format(self.hostname, end)
+            result = requests.post(endpoint, json=pl, timeout=ttl)
+
+        return result
